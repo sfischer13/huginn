9e973c2a3327759ae7410baa8df74b17c5d20037
Stefan Fischer <sfischer13@ymail.com>
2020-07-26 08:07:18 +0000

    Fix autoloader issue

--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,9 @@ module Huginn
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
+    # https://github.com/huginn/huginn/issues/2845
+    config.autoloader = :classic
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
